62888a9c79178cde05ee4e7c167d0782c371272d
Behlül Samil Selcuk <215264789+behluelsamilselcuk@users.noreply.github.com>
2025-10-06 22:02:27 +0200

    Erstelle Bedinungsfunktion, um Operation zu wählen
    Erstelle die Funktion "operate" und stelle
    Konditionen mit der switch-Anweisung auf, die je
    nach Bedingung die zugehörige Operatorfunktion
    auf die num1 & num2 ausführt

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// 
+// Variables
 let num1;
 let num2;
 let operator;
@@ -22,3 +22,20 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
+// 
+function operate(operator, num1, num2) {
+    switch(operator) {
+        case '+':
+            add(num1, num2);
+            break;
+        case '-':
+            subtract(num1, num2);
+            break;
+        case '*':
+            multiply(num1, num2);
+            break;
+        case '/':
+            divide(num1, num2);
+            break;
+    }
+}
