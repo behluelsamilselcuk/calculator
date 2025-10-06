4a0c2511262d157a97fe75a7e77beef7d59ca53b
Behlül Samil Selcuk <215264789+behluelsamilselcuk@users.noreply.github.com>
2025-10-06 21:39:30 +0200

    Erstelle Funktionen für math. Operatoren
    Erstellen von Additions-, Subtraktions-,
    Multiplikations- und Divisionsfunktionen mit einem
    Rückgabewert.

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 'use strict';
 
+// Functions for mathematical operators
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
