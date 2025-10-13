9ea3fc0f95f7eeaf2c7c63e3693f64b37679cea8
Behlül Samil Selcuk <215264789+behluelsamilselcuk@users.noreply.github.com>
2025-10-13 20:00:22 +0200

    Bringe den Taschenrechner zum Laufen

--- script.js
+++ script.js
@@ -2,50 +2,78 @@
 
 // Variables
 let num1 = '';
-let num2;
-let operator;
+let num2 = '';
+let operator = '';
 
 // Functions for mathematical operators
 function add(num1, num2) {
-    return num1 + num2;
+    return +num1 + +num2;
 }
 
 function subtract(num1, num2) {
-    return num1 - num2;
+    return +num1 - +num2;
 }
 
 function multiply(num1, num2) {
-    return num1 * num2;
+    return +num1 * +num2;
 }
 
 function divide(num1, num2) {
-    return num1 / num2;
+    return +num1 / +num2;
 }
 
 // Function to perform an operation on the numbers num1 and num2
 function operate(operator, num1, num2) {
-    switch(operator) {
-        case '+':
-            add(num1, num2);
-            break;
-        case '-':
-            subtract(num1, num2);
-            break;
-        case '*':
-            multiply(num1, num2);
-            break;
-        case '/':
-            divide(num1, num2);
-            break;
+    if (operator === '+') {
+        return add(num1, num2);
+    }
+    else if (operator === '-') {
+        return subtract(num1, num2);
+    }
+    else if (operator === 'x') {
+        return multiply(num1, num2);
+    }
+    else if (operator === '/') {
+        return divide(num1, num2);
     }
 }
 
 const numBtns = document.querySelectorAll('.number-button');
 const display = document.querySelector('#display');
+const opBtns = document.querySelectorAll('.operator-button')
+const equBtn = document.querySelector('#equal-button');
 
 numBtns.forEach(btn => {
     btn.addEventListener('click', () => {
-        num1 += btn.textContent;
-        display.textContent = num1;
+        if (operator === '') {
+            display.textContent = '';
+            num1 += btn.textContent;
+            display.textContent = num1;
+        }
+        else if (operator !== '') {
+            display.textContent = '';
+            num2 += btn.textContent;
+            display.textContent = num1 + ' ' + operator + ' ' + num2;
+        }
+        
+    })
+})
+
+opBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        if (num1 !== '') {
+            display.textContent = '';
+            operator = btn.textContent;
+            display.textContent = num1 + ' ' + operator;
+        }
     })
 });
+
+equBtn.addEventListener('click', () => {
+    display.textContent = '';
+    const result = operate(operator, num1, num2);
+    display.textContent = result;
+    operator = '';
+    num1 = '';
+    num2 = '';
+})
